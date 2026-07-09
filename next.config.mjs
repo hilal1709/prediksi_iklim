226c87ced0a7286d1aa3afa63925860e1dded810
hilal1709 <hilal.andi123@gmail.com>
2026-07-09 10:52:52 +0700

    fix value

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		const backendOrigin = process.env.BACKEND_URL;
+
+		return backendOrigin
+			? [
+					{
+						source: '/api/backend/:path*',
+						destination: `${backendOrigin}/:path*`,
+					},
+				]
+			: [];
+	},
+};
 
 export default nextConfig;
